e4f71916a5b1e1159b7a3c383b07000071c2cdd6
Jung Yeon <jyyang5542@gmail.com>
2024-08-05 15:07:18 +0900

    build: head 관련 정보 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/assets/scss/style.scss'],
+  app: {
+    head: {
+      title: 'My Nuxt App',
+      meta: [
+        { name: 'description', content: 'This is my Nuxt app.' },
+        { name: 'keywords', content: 'nuxt, vue, javascript' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  // css: ['~/assets/scss/style.scss'],
 });
