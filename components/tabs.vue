dea43084ca00394beb84fb5b24a80252f23977c5
Jung Yeon <jyyang5542@gmail.com>
2024-08-05 15:24:49 +0900

    feat: 탭 컴포넌트 작성

---
+++ components/tabs.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="(item, i) in list" :key="i">
+        <a :href="item.href" :title="item.title">{{ item.name }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    list: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+  display: flex;
+  gap: 30px;
+
+  li {
+    padding: 0 10px;
+
+    a {
+      font-size: 24px;
+    }
+  }
+}
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
     <h1>About</h1>
+    <tabs :list="tabItems" />
   </div>
 </template>
 
 <script>
+import Tabs from '~/components/tabs.vue';
 export default {
   data() {
-    return {};
+    return {
+      tabItems: [
+        { href: './about', name: 'tab-1', title: 'tab-1로 이동' },
+        { href: './about', name: 'tab-2', title: 'tab-2로 이동' },
+        { href: './about', name: 'tab-3', title: 'tab-3로 이동' },
+      ],
+    };
   },
 
   methods: {
