b2e081e3351a986fa0e826028d49dc79e22d6933
Jung Yeon <jyyang5542@gmail.com>
2024-08-06 10:38:09 +0900

    style: swiper 네비게이션 버튼 노출

--- pages/Swiper/index.vue
+++ pages/Swiper/index.vue
@@ -3,7 +3,7 @@
     <h1 class="sec__title">Swiper</h1>
     <Gnb />
 
-    <swiper :modules="[Navigation, Pagination]" navigation :pagination="{ clickable: true }" :slides-per-view="1" :space-between="10" class="mySwiper">
+    <swiper :modules="[Navigation, Pagination]" loop navigation :pagination="{ clickable: true }" :slides-per-view="1" :space-between="10" class="mySwiper">
       <swiper-slide v-for="index in 6" class="sec__cont active">Slide {{ index }}</swiper-slide>
     </swiper>
   </section>
@@ -14,3 +14,14 @@ import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 </script>
+
+<style lang="scss" scoled>
+.swiper-button-prev,
+.swiper-button-next {
+  &::before,
+  &::after {
+    font-size: 15px;
+    font-family: Arial, Georgia, 'Times New Roman', Times, serif !important;
+  }
+}
+</style>
