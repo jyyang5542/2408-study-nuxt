b81b8e1b17860bd609df521fd57f84af408a5527
Jung Yeon <jyyang5542@gmail.com>
2024-08-06 11:45:42 +0900

    feat: API 케이스 추가

--- pages/MyAPI/index.vue
+++ pages/MyAPI/index.vue
@@ -1,12 +1,19 @@
 <template>
   <section class="sec">
-    <h1 class="sec__title">Axios</h1>
+    <h1 class="sec__title">MyAPI</h1>
     <Gnb />
     <div class="sec__cont active">
       <div v-if="loading">Loading...</div>
-      <ul v-else>
-        <li v-for="item in list" :key="item.id">id: {{ item.id }} | {{ item.title }} | message: {{ item.message }}</li>
-      </ul>
+      <div v-else class="box">
+        <ul class="box__list">
+          <li class="box__title">String 호출 CASE</li>
+          <li v-for="item in list" :key="item.id" class="box__item">{{ item.el }}</li>
+        </ul>
+        <ul class="box__list">
+          <li class="box__title">HTML 호출 CASE</li>
+          <li v-for="item in html" :key="item.id" class="box__item" v-html="item.el"></li>
+        </ul>
+      </div>
       <div v-if="error" class="error">{{ error }}</div>
     </div>
   </section>
@@ -14,6 +21,7 @@
 
 <script setup>
 const list = ref([]);
+const html = ref([]);
 const loading = ref(true);
 const error = ref(null);
 
@@ -25,6 +33,7 @@ onMounted(async () => {
     }
     const data = await response.json();
     list.value = data.list;
+    html.value = data.html;
   } catch (err) {
     error.value = err.message;
   } finally {
@@ -36,18 +45,49 @@ onMounted(async () => {
 <style lang="scss">
 .sec__cont {
   flex-direction: column;
+}
+.box {
+  display: flex;
+  gap: 20px;
+  width: 100%;
 
-  ul {
+  &__list {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 100%;
     margin-top: 10px;
+    padding: 30px;
+    background: #fff;
+  }
+  &__title {
+    width: 100%;
+    padding-bottom: 10px;
+    margin-bottom: 20px;
+    font-weight: bold;
+    text-align: center;
+    border-bottom: 1px dotted #ddd;
+  }
+  &__item {
+    width: 100%;
+    line-height: 30px;
+    padding: 5px;
+    font-size: 18px;
+    text-align: center;
+    background-color: #eee;
+    border-radius: 10px;
 
-    li {
-      text-align: center;
-      font-size: 18px;
-
-      @media screen and (max-width: 750px) {
-        font-size: 16px;
-      }
+    @media screen and (max-width: 750px) {
+      font-size: 16px;
+    }
+    & + & {
+      margin-top: 10px;
+    }
+    * {
+      margin: 0;
+      padding: 0;
+      line-height: inherit;
     }
   }
 }
--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,8 +1,24 @@
 export default defineEventHandler(event => {
   return {
     list: [
-      { id: 0, title: 'Name', message: 'Hi' },
-      { id: 1, title: 'Title', message: 'Hello' },
+      { id: 0, el: '<h1>This is my HTML</h1>' },
+      { id: 1, el: '<h2>This is my HTML</h2>' },
+      { id: 2, el: '<h3>This is my HTML</h3>' },
+      { id: 3, el: '<h4>This is my HTML</h4>' },
+      { id: 4, el: '<h5>This is my HTML</h5>' },
+      { id: 5, el: '<h6>This is my HTML</h6>' },
+      { id: 6, el: '<strong>This is my HTML</strong>' },
+      { id: 7, el: '<p>This is my HTML</p>' },
+    ],
+    html: [
+      { id: 0, el: '<h1>This is my HTML</h1>' },
+      { id: 1, el: '<h2>This is my HTML</h2>' },
+      { id: 2, el: '<h3>This is my HTML</h3>' },
+      { id: 3, el: '<h4>This is my HTML</h4>' },
+      { id: 4, el: '<h5>This is my HTML</h5>' },
+      { id: 5, el: '<h6>This is my HTML</h6>' },
+      { id: 6, el: '<strong>This is my HTML</strong>' },
+      { id: 7, el: '<p>This is my HTML</p>' },
     ],
   };
 });
