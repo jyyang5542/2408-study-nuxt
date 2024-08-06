96390b0556704d0ec2f2c5c1d20b21c6b5409455
Jung Yeon <jyyang5542@gmail.com>
2024-08-06 10:40:58 +0900

    feat: Swiper Autoplay 기능 추가

--- pages/Swiper/index.vue
+++ pages/Swiper/index.vue
@@ -3,7 +3,7 @@
     <h1 class="sec__title">Swiper</h1>
     <Gnb />
 
-    <swiper :modules="[Navigation, Pagination]" loop navigation :pagination="{ clickable: true }" :slides-per-view="1" :space-between="10" class="mySwiper">
+    <swiper :modules="[Navigation, Pagination, Autoplay]" :autoplay="{ delay: 2000 }" loop navigation :pagination="{ clickable: true }" :slides-per-view="1" :space-between="10" class="mySwiper">
       <swiper-slide v-for="index in 6" class="sec__cont active">Slide {{ index }}</swiper-slide>
     </swiper>
   </section>
@@ -11,7 +11,7 @@
 
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 </script>
 
